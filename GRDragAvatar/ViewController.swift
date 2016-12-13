5206b29ae8b8c70981bad1a52902cc7c261c826a
chrong <guangr25@icloud.com>
2016-12-13 15:45:05 +0800

    Update ViewController.swift

--- GRDragAvatar/ViewController.swift
+++ GRDragAvatar/ViewController.swift
@@ -13,12 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let image = UIImage.init(named: "background")
-        view.backgroundColor = UIColor.init(patternImage: image!)
+        let image = UIImage(named: "background")
+        view.backgroundColor = UIColor(patternImage: image!)
         
-        let avatar = GRImageView.init(frame:
-            CGRect.init(x: (view.frame.size.width-80)/2, y: 64, width: 80, height: 80))
-        avatar.image = UIImage.init(named: "avatar")
+        let avatar = GRImageView(frame:
+            CGRect(x: (view.frame.size.width-80)/2, y: 64, width: 80, height: 80))
+        avatar.image = UIImage(named: "avatar")
         view .addSubview(avatar)
         avatar.isUserInteractionEnabled = true
         avatar.addDrag()
